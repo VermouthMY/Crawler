ce479fee729ab91eb98944b28b12fa0a897db6fa
VermouthMY <zero@zerodeMacBook-Pro.local>
2017-03-11 21:28:23 +0800

    Add getFoodInfo
    抓取具体商品

--- test.py
+++ test.py
@@ -8,7 +8,8 @@ import re
 
 shopId = []
 
-def getInfo(url):
+#抓取商家
+def getRestaurantInfo(url):
 	content=urllib2.urlopen(url).read()
 	typeEncode = sys.getfilesystemencoding()
 	html = content.decode('utf-8').encode(typeEncode)
@@ -23,14 +24,31 @@ def getInfo(url):
 	items = re.findall(pattern,html)
 	# 0=优惠 1=人均消费 2=店名 3=配送时间 4=配送费 5=评分 6=月销量
 	for item in items:
-		# print item[0],item[1],"id："+item[2],item[3],item[4]+"分钟",item[5],"评分："+item[6],"月销售："+item[7]
+		print item[0],item[1],"id："+item[2],item[3],item[4]+"分钟",item[5],"评分："+item[6],"月销售："+item[7]
 		shopId.append(item[2])
 
+#抓取详细菜品信息
+def getFoodInfo(shopId):
+	print "餐厅id："+shopId
+	url = "https://mainsite-restapi.ele.me/shopping/v2/menu?restaurant_id="+shopId
+	content=urllib2.urlopen(url).read()
+	typeEncode = sys.getfilesystemencoding()
+	html = content.decode('utf-8').encode(typeEncode)
+	pattern = re.compile('{},\"name\":(.*?),.*?'+
+				'\"recent_rating\":(.*?),.*?'+
+				'\"price\":(.*?),.*?'+
+				'\"recent_popularity\":(.*?),.*?',re.S)
+	items = re.findall(pattern,html)
+	# 名字、评分、价格、月销量
+	# 0=名字 1=评分 2=价格 3=月销量
+	for item in items:
+		print item[0],"评分："+item[1],"价格："+item[2],"月销售："+item[3]
 
 print "start geting Info..."
-getInfo("https://mainsite-restapi.ele.me/shopping/restaurants?extras%5B%5D=activities&geohash=wtw3qyjk6kx&latitude=31.19094&limit=24&longitude=121.59301&offset=0&terminal=web")
-print shopId
-
+getRestaurantInfo("https://mainsite-restapi.ele.me/shopping/restaurants?extras%5B%5D=activities&geohash=wtw3qyjk6kx&latitude=31.19094&limit=24&longitude=121.59301&offset=0&terminal=web")
+#print shopId[0]
+#1560660 进行测试
+#getFoodInfo('1560660')
 
 # fp=open(‘login.html’,’w’)
 # fp.write(content)
@@ -49,5 +67,5 @@ print shopId
 # https://mainsite-restapi.ele.me/shopping/restaurants?extras%5B%5D=activities&geohash=wtw3qyjk6kx&latitude=31.19094&limit=24&longitude=121.59301&offset=48&terminal=web
 # https://mainsite-restapi.ele.me/shopping/restaurants?extras%5B%5D=activities&geohash=wtw3qyjk6kx&latitude=31.19094&limit=24&longitude=121.59301&offset=72&terminal=web
 
-
+# https://mainsite-restapi.ele.me/shopping/v2/menu?restaurant_id=
 
